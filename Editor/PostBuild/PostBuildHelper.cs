ae39f28af6aee8c468b8157718eee8039eaf7abe
kfblake <keith@playvue.com>
2018-06-07 15:09:18 -0400

    iOS Auto-Increment Build Number
    Auto-Increment iOS build number AFTER each build (i.e. build X send to
    Xcode, X+1 set in Unity Player Settings for next build).

---
+++ Editor/PostBuild/PostBuildHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using UnityEngine;
+
+public static class PostBuildHelper
+{
+	[PostProcessBuild]
+	public static void OnBuildComplete(BuildTarget buildTarget, string pathToBuiltProject)
+	{
+		if (buildTarget != BuildTarget.iOS)
+		{
+			return;
+		}
+
+		IncrementBuildNumber();
+	}
+
+	private static void IncrementBuildNumber()
+	{
+		// Load the PlayerSettings asset.
+		var playerSettings = Resources.FindObjectsOfTypeAll<PlayerSettings>().FirstOrDefault();
+
+		if (playerSettings != null)
+		{
+
+			string currentValue = PlayerSettings.iOS.buildNumber;
+			int ver = 0;
+			if (int.TryParse (currentValue, out ver)) {
+				PlayerSettings.iOS.buildNumber = (ver + 1).ToString ();
+			}
+
+		}
+	}
+}
